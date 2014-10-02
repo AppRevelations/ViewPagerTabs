40cb3fceedd56d38668944efe4bb45224622abad
Himanshu Lakra <himanshu.lakra10@gmail.com>
2014-10-02 22:27:41 +0530

    Initial commit

---
+++ src/com/apprevelations/viewpagertabs/TabsPagerAdapter.java
@@ -0,0 +1,38 @@
+package com.apprevelations.viewpagertabs;
+
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class TabsPagerAdapter extends FragmentPagerAdapter {
+
+	public TabsPagerAdapter(FragmentManager fm) {
+		super(fm);
+	}
+
+	@Override
+	public Fragment getItem(int index) {
+
+		switch (index) {
+		case 0:
+			// Tab1 fragment activity
+			return new Tab1();
+		case 1:
+			// Tab2 fragment activity 
+			return new Tab2();
+		case 2:
+			// Tab3 fragment activity
+			return new Tab3();
+		}
+
+		return null;
+	}
+
+	@Override
+	public int getCount() {
+		// get item count - equal to number of tabs
+		return 3;
+	}
+
+}
